03ecda285e16684847e6070e9c9fa1b32e10f182
Mario Ibarra <marioibarra1003@gmail.com>
2025-08-01 18:44:24 -0600

    Modificacion del diseño del logo, inclusion de carpeta de assets y modificacion de dependencias

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import '../config/supabase_config.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>{
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
 
   Future<bool> verificarUsuario(String email, String password) async {
-    try{
+    try {
       print('Buscando usuario con email: $email y password: $password');
 
       final response = await SupabaseConfig.client
@@ -23,7 +24,7 @@ class _LoginScreenState extends State<LoginScreen>{
       print('Respuesta de la base de datos: $response');
 
       return response.length > 0;
-    }catch(e){
+    } catch (e) {
       print('error al verificar usuario: $e');
       return false;
     }
@@ -33,82 +34,134 @@ class _LoginScreenState extends State<LoginScreen>{
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Tienda Cinammons',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.1,
+            vertical: 50,
+          ),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: double.infinity, // Máximo 400px de ancho
+            ),
+            child: Card(
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(50.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CircleAvatar(
+                      radius: MediaQuery.of(context).size.width * 0.07,
+                      backgroundImage: AssetImage('assets/images/logo.jpg'),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      'Tienda Cinammons',
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.02,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    SizedBox(height: 20),
+
+                    TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.password),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 30),
+
+                    SizedBox(
+                      width: 300,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        onPressed: isLoading
+                            ? null
+                            : () async {
+                                setState(() {
+                                  isLoading = true;
+                                });
+                                String email = emailController.text;
+                                String password = passwordController.text;
+
+                                if (email.isEmpty || password.isEmpty) {
+                                  setState(() {
+                                    isLoading = false;
+                                  });
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text('Ingrese todos los campos'),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                  );
+                                  return;
+                                }
+
+                                bool usuarioValido = await verificarUsuario(
+                                  email,
+                                  password,
+                                );
+
+                                if (usuarioValido) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text('Login Exitoso'),
+                                      backgroundColor: Colors.green,
+                                    ),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                        'Email o contraseña incorrectos',
+                                      ),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                  );
+                                }
+                                setState(() {
+                                  isLoading = false;
+                                });
+                              },
+                        child: isLoading
+                            ? CircularProgressIndicator(color: Colors.white)
+                            : Text(
+                                'Iniciar Sesion',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            SizedBox(height: 40),
-
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 30),
-
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                  onPressed: () async{
-                    String email = emailController.text;
-                    String password = passwordController.text;
-
-                    if (email.isEmpty || password.isEmpty){
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content:
-                        Text('Ingrese todos los campos'),
-                        backgroundColor: Colors.red,
-                        ),
-                      );
-                      return;
-                    }
-
-                    bool usuarioValido = await verificarUsuario(email, password);
-
-                    if(usuarioValido) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content:
-                        Text('Login Exitoso'),
-                          backgroundColor: Colors.green,
-                        ),
-                      );
-                    }else{
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content:
-                        Text('Email o contraseña incorrectos'),
-                          backgroundColor: Colors.red,
-                        ),
-                      );
-                    }
-                  },
-                  child: Text('Iniciar Sesion',
-                  style: TextStyle(fontSize: 20),)
-              )
-            )
-          ],
+          ),
         ),
       ),
     );
   }
-
 }
